0d2fb4a82f1ada3b519580232851c2d8e236bb0a
Iniestable <86682836+Iniestable@users.noreply.github.com>
2024-01-31 20:15:27 +0100

    Add files via upload

--- For.py
+++ For.py
@@ -4,11 +4,13 @@ from time import sleep
 sense = SenseHat()
 Dic1 = ["Agua", 12, 2]
 Dic2 = ["Culo", 23, 7]
+apagar = False
 
 # Define some colours
 R = (255, 0, 0)
 G = (0, 255, 0)
 W = (255, 255, 255)
+A = (0, 0, 0)
 
 # Set up where each colour will display
 steve_pixels = [
@@ -29,11 +31,15 @@ steve_pixels[Dic2[2]] = R
 
 sense.set_pixels(steve_pixels)  #Primero se ponen en este color
 
-for i in steve_pixels:
-  if i[0] == 0 and i[1] != 0:
-    steve_pixels[counter] = R
-  counter = counter + 1
-  
-sleep(1)
-sense.set_pixels(steve_pixels)  #Luego todos rojos
+while(1):
+    if Dic1[0] == "Agua":
+        if apagar == True:
+            steve_pixels[Dic1[2]] = R
+            apagar = False
+        else:
+            steve_pixels[Dic1[2]] = A
+            apagar = True
+    
+    sleep(1)
+    sense.set_pixels(steve_pixels)  #Luego todos rojos
 
