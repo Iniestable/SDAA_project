587cb4569035e94068a62b0a093a39678e591d5d
Iniestable <86682836+Iniestable@users.noreply.github.com>
2024-02-02 17:18:19 +0100

    Add files via upload

--- Lectura.py
+++ Lectura.py
@@ -1,7 +1,9 @@
 from sense_hat import SenseHat
 
 sense = SenseHat()
-humidity = sense.get_humidity()
-print(humidity)
-temp = sense.get_temperature()
-print(temp)
+
+humidity = round(sense.get_humidity(), 2)
+humedad = str(humidity) + "% de humedad"
+print(humedad)
+temp = round(sense.get_temperature(), 2)    #Para redondear a dos cifras la lectura
+print(str(temp) + "ºC")
